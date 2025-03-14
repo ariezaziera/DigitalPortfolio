877cb96a901f19e06f0fa1537b93d9a7e7f8c9f3
ariezaaziera <ariezaziera@gmail.com>
2025-03-14 17:51:51 +0800

    Add button tap effect

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -58,7 +58,8 @@ const ContactSection = () => {
           <motion.button
             type="submit"
             className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-6 rounded-lg transition duration-300"
-            whileTap={{ scale: 0.9 }} // Shrink slightly when clicked
+            onTapStart={() => console.log("Tapped!")} // Debugging to see if tap is detected
+            whileTap={{ scale: 0.9 }} // Shrinks the button when tapped
           >
             Send Message
           </motion.button>
