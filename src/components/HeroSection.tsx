4a4b44252b4bf584e9a1b61afc8ec8e438916a26
Arieza Aziera <152407762+ariezaziera@users.noreply.github.com>
2026-04-03 17:07:04 +0800

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -81,7 +81,7 @@ const HeroSection = () => {
             className="relative w-[300px] h-[300px] overflow-hidden"
           >
             <Image
-              src="/profile8.png"
+              src="/profile1.png"
               alt="Profile Picture"
               width={300}
               height={300}
