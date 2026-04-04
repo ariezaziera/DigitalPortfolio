d3f29d96fb20eb6e5a22fea6c44c7881f4c859fd
Arieza Aziera <152407762+ariezaziera@users.noreply.github.com>
2026-04-04 11:23:22 +0800

    Update Resume Section.tsx

--- src/components/Resume Section.tsx
+++ src/components/Resume Section.tsx
@@ -186,8 +186,8 @@ const ResumeSection = () => {
                   <div className="absolute -left-7 top-6 w-4 h-4 bg-purple-500 rounded-full border-2 border-gray-900"></div>
 
                   {/* 🟢 Mobile-Friendly Layout */}
-                  <div className="flex flex-col md:flex-row md:items-center gap-2">
-                    <div className="relative h-14 w-14 flex-shrink-0 flex items-center justify-center mb-5 md:mr-5 md:ml-3">
+                  <div className="flex flex-col md:flex-row md:items-start gap-2">
+                    <div className="relative h-14 w-14 self-start flex-shrink-0 flex items-center justify-center mb-5 md:mb-0 md:mr-5 md:ml-3">
                       {/* Glow belakang */}
                       <div className="h-16 w-16 flex-shrink-0 flex items-center justify-center rounded-xl bg-white/35 shadow-[0_0_20px_rgba(255,255,255,0.08)] md:mr-4">
                         <Image
